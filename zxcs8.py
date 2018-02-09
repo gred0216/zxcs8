79e72432dacbd97f1376b1b4c616b85f1bbb072c
gred0216 <likuan1222@gmail.com>
2018-02-10 04:58:52 +0800

    exception handling

--- zxcs8.py
+++ zxcs8.py
@@ -6,21 +6,32 @@ import os
 
 def get_book_links(sortlink):
 	# retrieve all book link from the category
-	r = requests.get(sortlink)
-	to_get = sortlink + '/page/'
-	soup = BeautifulSoup(r.text)
-	pages = soup.find(id='pagenavi')
-	last_page = pages.find_all('a')[-1].get('href')
-	books = []
-	last_page_num = int(re.search('page/([0-9]*)',last_page)[1])
-	for i in range(1,last_page_num+1):
-		currect_page = to_get + str(i)
-		c = requests.get(currect_page)
-		soup2 = BeautifulSoup(c.text)
-		alldt = soup2.find_all('dt')
-		for booklink in alldt:
-			books.append(booklink.a.get('href'))
-	return books
+	try:
+		r = requests.get(sortlink)
+	except Exception as err:
+		return 'Unexpected Error', err
+	else:
+		soup = BeautifulSoup(r.text)
+		if not soup.ok:
+			return str(soup.status_code) + ' error'
+		else:
+			pages = soup.find(id='pagenavi')
+			last_page = pages.find_all('a')[-1].get('href')
+			book_links = []
+			failed_page =[]
+			last_page_num = int(re.search('page/([0-9]*)',last_page)[1])
+			to_get = sortlink + '/page/'
+			for i in range(1,last_page_num+1):
+				currect_page = to_get + str(i)
+				c = requests.get(currect_page)
+				if not c.ok:
+					failed_page.append(currect_page)
+				else:
+					soup2 = BeautifulSoup(c.text)
+					all_dt = soup2.find_all('dt')
+					for booklink in all_dt:
+						books.append(booklink.a.get('href'))
+			return book_links, failed_page
 
 def get_book_info(page):
 	# retrieve the voting evaluation of the book, donwload page link and title
@@ -51,8 +62,8 @@ class Book:
 	def download(self):
 		try:
 			g = requests.get(self.link)
-		except:
-			return 'link error'
+		except Exception as err:
+			return 'Unexpected Error', err
 		else:
 			if not g.ok:
 				return str(g.status_code) + 'error'
@@ -61,8 +72,7 @@ class Book:
 				spans = [ x.a for x in dlsoup.find_all('span') ]
 				dllinks = [y.get('href') for y in spans if y]				
 		for i in dllinks:
-			dl = requests.get(i)
-			save = False
+			dl = requests.get(i)			
 			if dl.ok:
 				break
 			elif i == dllinks[-1]:
@@ -70,6 +80,30 @@ class Book:
 		os.makedirs('./download/',exist_ok=True)
 		with open('./download/'+self.name+'.rar','wb') as f:
 			f.write(dl.content)
+
+main_shelf = []
+
+def create_book(info):
+	return Book(info[0],info[1],info[2],info[3],info[4],info[5],info[6])
+
+def add_book(book,shelf):
+	if book not in shelf:
+		shelf.append(book)
+	
+def delete_book(book,shelf):
+	try:
+		shelf.remove(book)
+	except ValueError as err:
+		return book.name + ' is not in the shelf'
+
+def create_category_shelf(category):
+	infos = get_book_links(category[0])[0]
+	category[1]
+	for links in infos[0]:
+		pass
+
+
+
 
 
 
