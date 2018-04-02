331f81faca721dca95b60d98fa4ccfdaff0ff445
gred0216 <likuan1222@gmail.com>
2018-04-03 03:33:12 +0800

    rewrite sorted result saving. add download top book method

--- sorter.py
+++ sorter.py
@@ -1,113 +1,73 @@
 from zxcs8 import *
 from glob import glob
 import os
-import numpy as np
+import math
 
 
 all_tag = glob('./tags/*.txt')
 all_sort = glob('./sort/*.txt')
-all_txt = all_tag + all_sort
+logger = logging.getLogger('zxcs8')
 
 
 def sort_score(score_list):
     score_list.sort(key=lambda tup: tup[1], reverse=True)
 
 
-def sort_by_excellent(shelf, path):
-    if 'tags' in path:
-        if not os.path.isdir('./score/excellent/tags'):
-            os.makedirs('./score/excellent/tags', exist_ok=True)
-    elif 'sort' in path:
-        if not os.path.isdir('./score/excellent/sort'):
-            os.makedirs('./score/excellent/sort', exist_ok=True)
-
-    excellent = []
+def save_sort_by_excellent(shelf):
+    rank = []
     for book in shelf.content:
-        excellent.append((book, int(shelf.content[book]['score1'])))
-    sort_score(excellent)
+        rank.append((book, int(shelf.content[book]['score1'])))
+    sort_score(rank)
 
-    with open('./score/excellent/{}{}_excellent.txt'.format(path, shelf.name),
-              'w', encoding='UTF-8') as f:
-        f.write('\n'.join('{}:{}'.format(tup[0], tup[1]) for tup in excellent))
+    return('excellent', shelf.name, rank,)
 
 
-def sort_by_bad(shelf, path):
-    if 'tags' in path:
-        if not os.path.isdir('./score/bad/tags'):
-            os.makedirs('./score/bad/tags', exist_ok=True)
-    elif 'sort' in path:
-        if not os.path.isdir('./score/bad/sort'):
-            os.makedirs('./score/bad/sort', exist_ok=True)
-
-    excellent = []
+def save_sort_by_bad(shelf):
+    rank = []
     for book in shelf.content:
-        excellent.append((book, int(shelf.content[book]['score1'])))
-    sort_score(excellent)
+        rank.append((book, int(shelf.content[book]['score5'])))
+    sort_score(rank)
 
-    with open('./score/bad/{}{}_bad.txt'.format(path, shelf.name),
-              'w', encoding='UTF-8') as f:
-        f.write('\n'.join('{}:{}'.format(tup[0], tup[1]) for tup in excellent))
+    return('bad', shelf.name, rank,)
 
 
-def sort_by_ratio(shelf, path):
-    if 'tags' in path:
-        if not os.path.isdir('./score/ratio/tags'):
-            os.makedirs('./score/ratio/tags', exist_ok=True)
-    elif 'sort' in path:
-        if not os.path.isdir('./score/ratio/sort'):
-            os.makedirs('./score/ratio/sort', exist_ok=True)
-
-    ratio = []
+def save_sort_by_ratio(shelf):
+    rank = []
     for book in shelf.content:
-        score = 0
+        ratio = 0
         try:
-            score = ((int(shelf.content[book]['score1']) +
+            ratio = ((int(shelf.content[book]['score1']) +
                       int(shelf.content[book]['score2'])) /
                      (int(shelf.content[book]['score4']) +
                       int(shelf.content[book]['score5'])))
         except ZeroDivisionError:
-            score = -1
-        ratio.append((book, round(score, 3)))
-    sort_score(ratio)
+            ratio = -1
+        rank.append((book, round(ratio, 3)))
+    sort_score(rank)
 
-    with open('./score/ratio/{}{}_ratio.txt'.format(path, shelf.name),
-              'w', encoding='UTF-8') as f:
-        f.write('\n'.join('{}:{}'.format(tup[0], tup[1]) for tup in ratio))
+    return('ratio', shelf.name, rank,)
 
 
-def sort_by_votes(shelf, path):
-    if 'tags' in path:
-        if not os.path.isdir('./score/votes/tags'):
-            os.makedirs('./score/votes/tags', exist_ok=True)
-    elif 'sort' in path:
-        if not os.path.isdir('./score/votes/sort'):
-            os.makedirs('./score/votes/sort', exist_ok=True)
-
-    votes = []
+def sort_by_votes(shelf):
+    rank = []
     for book in shelf.content:
         vote = (int(shelf.content[book]['score1']) +
                 int(shelf.content[book]['score2']) +
                 int(shelf.content[book]['score3']) +
                 int(shelf.content[book]['score4']) +
                 int(shelf.content[book]['score5']))
-        votes.append((book, vote))
-    sort_score(votes)
+        rank.append((book, vote))
+    sort_score(rank)
 
-    with open('./score/votes/{}{}_votes.txt'.format(path, shelf.name),
-              'w', encoding='UTF-8') as f:
-        f.write('\n'.join('{}:{}'.format(tup[0], tup[1]) for tup in votes))
+    return('votes', shelf.name, rank,)
 
 
-def sort_by_overall(shelf, path):
-    if 'tags' in path:
-        if not os.path.isdir('./score/overall/tags'):
-            os.makedirs('./score/overall/tags', exist_ok=True)
-    elif 'sort' in path:
-        if not os.path.isdir('./score/overall/sort'):
-            os.makedirs('./score/overall/sort', exist_ok=True)
-
+def sort_by_overall(shelf):
     rank = []
     for book in shelf.content:
+        # book_name = re.search('《.*》', book)[0]
+        book_name = book
+
         vote = (int(shelf.content[book]['score1']) +
                 int(shelf.content[book]['score2']) +
                 int(shelf.content[book]['score3']) +
@@ -118,25 +78,17 @@ def sort_by_overall(shelf, path):
                  -1 * int(shelf.content[book]['score4']) +
                  -2 * int(shelf.content[book]['score5']))
         try:
-            rank.append((book, round(score / vote, 3)))
+            rank.append((book_name,
+                         round(score / vote + math.log(vote, 1000), 3)))
         except ZeroDivisionError:
-            rank.append((book, -1))
+            rank.append((book_name, -1))
 
     sort_score(rank)
 
-    with open('./score/overall/{}{}_overall.txt'.format(path, shelf.name),
-              'w', encoding='UTF-8') as f:
-        f.write('\n'.join('{}:{}'.format(tup[0], tup[1]) for tup in rank))
+    return('overall', shelf.name, rank,)
 
 
-def sort_by_score(shelf, path):
-    if 'tags' in path:
-        if not os.path.isdir('./score/score/tags'):
-            os.makedirs('./score/score/tags', exist_ok=True)
-    elif 'sort' in path:
-        if not os.path.isdir('./score/score/sort'):
-            os.makedirs('./score/score/sort', exist_ok=True)
-
+def sort_by_score(shelf):
     rank = []
     for book in shelf.content:
         score = (2 * int(shelf.content[book]['score1']) +
@@ -146,24 +98,60 @@ def sort_by_score(shelf, path):
         rank.append((book, score))
     sort_score(rank)
 
-    with open('./score/score/{}{}_score.txt'.format(path, shelf.name),
-              'w', encoding='UTF-8') as f:
+    return('score', shelf.name, rank,)
+
+
+def save_score(rank_type, name, rank, path):
+    if 'tags' in path:
+        if not os.path.isdir('./score/%s/tags' % rank_type):
+            os.makedirs('./score/%s/tags' % rank_type)
+    elif 'sort' in path:
+        if not os.path.isdir('./score/%s/sort' % rank_type):
+            os.makedirs('./score/%s/sort' % rank_type)
+    else:
+        if not os.path.isdir('./score/%s/other' % rank_type):
+            os.makedirs('./score/%s/other' % rank_type)
+
+    with open('./score/{}/{}/{}_{}.txt'.format(
+        rank_type, path, name, rank_type),
+            'w', encoding='UTF-8') as f:
         f.write('\n'.join('{}:{}'.format(tup[0], tup[1]) for tup in rank))
+    logger.info('{} sorting result of {} saved'.format(rank_type, name))
+
+
+def download_top(shelf, rank, num, download_path):
+    book_num = shelf.get_book_num()
+    if book_num < num:
+        logger.warning('Only {} book(s) in the Shelf {}'
+                       .format(book_num, shelf.name))
+        num = book_num
+    for i in range(num):
+        shelf.content[rank[i][0]].download(path=download_path)
+    logger.info(('Successfully downloaded top {} books of Shelf {}'
+                 .format(num, shelf.name)))
 
 
 if __name__ == '__main__':
+    logger = set_log()
+    logger.info('start logging')
+
     for txt in all_tag:
-        path = 'tags/'
+        path = 'tags'
         with open(txt, 'r', encoding='UTF-8') as f:
             text = f.read()
         shelf = from_json(text)
-        sort_by_score(shelf, path)
-        sort_by_overall(shelf, path)
+        overall = sort_by_overall(shelf)
+        save_score(*(overall + (path,)))
+        download_top(shelf, overall[2], 3, '{}/{}'.format(path, shelf.name))
 
     for txt in all_sort:
-        path = 'sort/'
+        path = 'sort'
         with open(txt, 'r', encoding='UTF-8') as f:
             text = f.read()
         shelf = from_json(text)
-        sort_by_score(shelf, path)
-        sort_by_overall(shelf, path)
+        overall = sort_by_overall(shelf)
+        save_score(*(overall + (path,)))
+        download_top(shelf, overall[2], 3, '{}/{}'.format(path, shelf.name))
+
+    logger.info('stop logging')
+    logging.shutdown()
